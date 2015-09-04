5c3d499bbd8b3324032686f6bb7debf5dbc5bb44
Lisa Cawley Ruiz <stellated@MacBook-Air.local>
2015-09-03 20:51:24 -0400

    Completed pset 1

--- psets/1.js
+++ psets/1.js
@@ -15,6 +15,6 @@ var cur_yr = 2015;
 var birth_yr = 1982;
 
 var age1 = cur_yr - birth_yr;
-var age2 = cur_yr - (birth_yr - 1);
+var age2 = (cur_yr - birth_yr) - 1;
 
 console.log('They are either ' + age1 + ' or ' + age2);
