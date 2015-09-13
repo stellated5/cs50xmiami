c45d0e3862bafa216be1fe4fab281983e1f1c7da
Lisa Cawley Ruiz <stellated@MacBook-Air.local>
2015-09-13 12:53:15 -0400

    Completed pset 7

--- psets/7.js
+++ psets/7.js
@@ -13,4 +13,11 @@ fixStart('babble'): 'ba**le'
 
 function fixStart(s) {
   // write your solution here...
+var regExInput = new RegExp(s[0], "g");
+var sub = s.substr(1, s.length);
+var rep = s[0] + sub.replace(regExInput, '*');
+
+console.log(rep);
 }
+//fixStart("live love laugh");
+fixStart("live love laugh");
